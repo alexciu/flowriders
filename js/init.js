af040665deb885d5a270fa06cd5d6474437f40b8
Alex Ciu <alexcm8686@yahoo.com>
2019-03-26 20:55:28 +0200

    updates

--- js/init.js
+++ js/init.js
@@ -3,7 +3,15 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $('.dropdown-trigger').dropdown();
+    $('.dropdown-trigger').dropdown({
+      constrainWidth : false, 
+      coverTrigger : false
+      });
+    $('.tap-target').tapTarget();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true,
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
