63b85c26696c9e8536fb403e55752d64732640ec
Alex Ciurea <Alex_Ciurea@SurveySampling.com>
2019-03-28 15:33:06 +0200

    carusel slick

--- js/init.js
+++ js/init.js
@@ -8,9 +8,17 @@
       coverTrigger : false
       });
     $('.tap-target').tapTarget();
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      indicators: true,
+    // $('.carousel.carousel-slider').carousel({
+    //   fullWidth: true,
+    //   indicators: true,
+    // });
+    $('.carousel_slick ').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
     });
 
   }); // end of document ready
