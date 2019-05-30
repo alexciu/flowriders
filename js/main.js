f1481e9e2b7b3d11f133aaab93ed06aff755f78a
Alex Ciurea <Alex_Ciurea@SurveySampling.com>
2019-05-30 10:33:21 +0300

    update event par

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $( document ).ready(function(){
 
 /* Cookies */
 $("#cookies").addClass("display");
-   $("#close-cookies").click(function(){ 
-    event.preventDefault();
+   $("#close-cookies").click(function(e){ 
+    e.preventDefault();
     $("#cookies").addClass("close-cookies");
   });
 
